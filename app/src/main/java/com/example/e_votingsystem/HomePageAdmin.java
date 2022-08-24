6ee57a4332307dfef3b0b29b6d3f58cba6f240f2
DarshanR06 <gdarshanr@gmail.com>
2022-08-24 20:42:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/e_votingsystem/HomePageAdmin.java
@@ -0,0 +1,64 @@
+package com.example.e_votingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class HomePageAdmin extends AppCompatActivity {
+    private LinearLayout logout;
+    private LinearLayout createElection;
+    private LinearLayout settingsPage;
+    private LinearLayout createCandidate;
+    private LinearLayout viewresults;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page_admin);
+        createElection=findViewById(R.id.createElectionAdmin);
+        settingsPage=findViewById(R.id.SettingsAdmin);
+        createCandidate=findViewById(R.id.candidateAdmin);
+        viewresults=findViewById(R.id.results);
+        logout=findViewById(R.id.logoutAdmin);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageAdmin.this, LoginPage.class));
+                Toast.makeText(HomePageAdmin.this,"Logout successfully",Toast.LENGTH_LONG).show();
+            }
+        });
+        createElection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageAdmin.this, CreateElectionPage.class));
+            }
+        });
+        createCandidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageAdmin.this, CandidateOption.class));
+            }
+        });
+
+        viewresults.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageAdmin.this, electiontype.class));
+            }
+        });
+
+        settingsPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageAdmin.this, SettingsAdmin.class));
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/e_votingsystem/electiontype.java
@@ -0,0 +1,73 @@
+
+package com.example.e_votingsystem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class electiontype extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    private adminadapter.RecyclerViewClickListener  listener;
+    private adminadapter adminadapter;
+    private ArrayList<Election> list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_create_candidate_page);
+
+        recyclerView=findViewById(R.id.electionList);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("Election");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        listener =new adminadapter.RecyclerViewClickListener() {
+            @Override
+            public void OnClick(View v, int position) {
+                Intent intent=new Intent(getApplicationContext(),Results.class);
+                intent.putExtra("electionName",list.get(position).getName());
+                startActivity(intent);
+            }
+        };
+        list=new ArrayList<>();
+        adminadapter = new adminadapter(this, list, listener);
+        recyclerView.setAdapter(adminadapter);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Election election=dataSnapshot.getValue(Election.class);
+                    list.add(election);
+                }
+                adminadapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        recyclerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(electiontype.this, Results.class));
+            }
+        });
+
+    }
+}
